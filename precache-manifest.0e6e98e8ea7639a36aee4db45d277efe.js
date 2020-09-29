a9a2c6d519c8838224a89f6402e60e0384cb6d26
smitpatel13 <smits.patel3@gmail.com>
2020-09-29 11:21:18 -0300

    Add files via upload

---
+++ precache-manifest.0e6e98e8ea7639a36aee4db45d277efe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9175880b4995efa1e75013936c6e6f5c",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "b792b0e5151bb0b8cc80",
+    "url": "/covid-19-tracker/static/css/main.75bbf4d4.chunk.css"
+  },
+  {
+    "revision": "f898f26e8490e35f0a6a",
+    "url": "/covid-19-tracker/static/js/2.9577bbe0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.9577bbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b792b0e5151bb0b8cc80",
+    "url": "/covid-19-tracker/static/js/main.b8f151d2.chunk.js"
+  },
+  {
+    "revision": "bdd759be6f307e7910dc",
+    "url": "/covid-19-tracker/static/js/runtime-main.3add0288.js"
+  },
+  {
+    "revision": "522dd832996e6111bd54f2ba64196a85",
+    "url": "/covid-19-tracker/static/media/logo.522dd832.png"
+  }
+]);
